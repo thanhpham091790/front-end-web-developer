e557f66896f80e3606bd9e68ba408c2ddaee9289
Thanh Pham <thanhpham091790@gmail.com>
2022-03-21 15:06:02 -0600

    Drawing image

--- javascript/1_canvas_template/script.js
+++ javascript/1_canvas_template/script.js
@@ -6,11 +6,8 @@ const ctx = canvas.getContext('2d');
 ctx.fillStyle = 'rgb(0,0,0)';
 ctx.fillRect(0,0,width,height);
 
-ctx.strokeStyle = 'white';
-ctx.lineWidth = 1;
-ctx.font = '36px arial';
-ctx.strokeText('Canvas text', 50, 50);
-
-ctx.fillStyle = 'red';
-ctx.font = '48px georgia';
-ctx.fillText('Canvas text', 50, 150);
+const image = new Image();
+image.src = 'firefox.png';
+image.addEventListener('load',()=>{
+    ctx.drawImage(image,20,20,185,175,50,50,185,175);
+});
