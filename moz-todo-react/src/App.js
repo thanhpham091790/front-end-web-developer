6e5e7ee1f960214f42d82fb5a69fa379d00a6ca6
Thanh Pham <thanhpham091790@gmail.com>
2022-07-12 00:19:54 -0600

    Rendering with iteration

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -3,6 +3,10 @@ import Todo from './components/Todo';
 
 function App(props) {
 
+  const taskList = props.tasks.map((task) => {
+    return <Todo id={task.id} name={task.name} completed={task.completed} />
+  });
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -48,9 +52,7 @@ function App(props) {
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-        <Todo name="Eat" completed={true} id="todo-0" />
-        <Todo name="Sleep" completed={false} id="todo-1" />
-        <Todo name="Repeat" completed={false} id="todo-2" />
+        {taskList}
       </ul>
     </div>
   );
